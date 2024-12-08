fcb91f0147991b3dff98933e88a4dc8e75b44f94
Ashraf Mahmoud <ashraf.mahmoud@talabat.com>
2024-12-08 17:17:48 +0200

    Testing Dart AI Review.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  final AAA = 'Testing Stuff';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
